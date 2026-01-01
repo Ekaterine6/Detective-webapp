367136a77630a175753e21262ff8cb51f1787136
Ekaterine <e.razmadze5@gmail.com>
2026-01-01 19:57:13 +0400

    updated settings but having issues with the terminal

--- darknews_project/settings.py
+++ darknews_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'final',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# for images
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ final/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pro_pic = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    bio = models.TextField(blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+    
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    media = models.FileField(upload_to="post_media/", blank=True, null=True)
+
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    tags = models.CharField(max_length=255, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
