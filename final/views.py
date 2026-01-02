cae4ea7dbdf0ad5531555d1ef2134b16882cffac
Ekaterine <e.razmadze5@gmail.com>
2026-01-02 22:45:39 +0400

    layed the base down for html pages and urls

--- final/views.py
+++ final/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by("-create_time")
+    return render(request, "news/index.html", {
+        "posts": posts
+    })
+
+@login_required
+def create_post(request):
+    return redirect("index")
+
+
