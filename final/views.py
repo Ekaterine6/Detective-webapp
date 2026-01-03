a83bd98bedcb5fb75daf43f7cdb259b0ad7cd84b
Ekaterine <e.razmadze5@gmail.com>
2026-01-03 18:05:41 +0400

    urls amd templates all work

--- final/views.py
+++ final/views.py
@@ -1,16 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 
 def index(request):
     posts = Post.objects.all().order_by("-create_time")
-    return render(request, "news/index.html", {
+    return render(request, "index.html", {
         "posts": posts
     })
 
 @login_required
 def create_post(request):
-    return redirect("index")
+    if request.method == "POST":
+        Post.objects.create(
+            author=request.user,
+            title=request.POST["title"],
+            body=request.POST["body"],
+            country=request.POST["country"],
+            city=request.POST.get("city", ""),
+            latitude=request.POST["latitude"],
+            longitude=request.POST["longitude"],
+            tags=request.POST.get("tags", "")
+        )
+        return redirect("index")
+    
+    return render(request, "create_post.html")
 
 
